686597f5b6608f189821553c2ee9b0ea417427f5
Lorenzo <32000123+lorenzo-deluca@users.noreply.github.com>
2025-02-02 18:10:32 +0100

    fix logs

--- components/bentel-kyo/bentel_kyo32.h
+++ components/bentel-kyo/bentel_kyo32.h
@@ -467,13 +467,13 @@ class Bentel_Kyo32 : public esphome::PollingComponent, public uart::UARTDevice, 
 				if (this->logTrace && (StatoZona == 1) != stato_sirena->state)
 					ESP_LOGI("stato_sirena", "Stato Sirena #1 %i", StatoZona);
 
+				stato_sirena->publish_state(StatoZona == 1);
+
 				StatoZona = ((Rx[10] >> 5) & 1);
-					ESP_LOGI("stato_sirena", "Stato Sirena #2 %i", StatoZona);
+				ESP_LOGI("stato_sirena", "Stato Sirena #2 %i", StatoZona);
 
 				StatoZona = ((Rx[10] >> 6) & 1);
-					ESP_LOGI("stato_sirena", "Stato Sirena #3  %i", StatoZona);
-				
-				stato_sirena->publish_state(StatoZona == 1);
+				ESP_LOGI("stato_sirena", "Stato Sirena #3  %i", StatoZona);
 				
 				// CICLO STATO USCITE
 				for (i = 0; i < 4; i++)
